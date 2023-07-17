75fb3843266c1d980da99a4660ee305549aa2803
skilful <skilfulgvmes@hotmail.com>
2023-07-18 01:04:51 +0200

    add styledcomponents, add basic style to home page

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
+import "./style/style.css";
 import router from "./router";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+let root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,109 @@
 import React from "react";
+import styled, { keyframes } from "styled-components";
+
+import { PageWrapper } from "../style/generic-styles";
+import gearFill from "../img/gear-fill.svg";
+import playCircle from "../img/play-circle.svg";
+import heartFill from "../img/heart.svg";
+
+const ButtonsContainer = styled.div`
+  position: relative;
+  z-index: 2;
+  display: flex;
+  flex-flow: column;
+  width: 100%;
+  height: 100%;
+  justify-content: space-around;
+`;
+
+const SVGSettingContainer = styled.div`
+  height: 30%;
+`;
+
+const SVGSetting = styled.img`
+  width: 36px;
+  margin-left: 3%;
+  padding: 2%;
+  border: 2px solid #ff9233;
+  border-radius: 5px;
+`;
+
+const SVGPlayContainer = styled.div`
+  display: flex;
+  width: 100%;
+  height: 30%;
+  align-items: center;
+  justify-content: center;
+`;
+
+const SVGPlay = styled.img`
+  padding: 1.5%;
+  width: 36px;
+  height: 36px;
+  border: 1.5px solid #ff9233;
+  border-radius: 50%;
+`;
+
+const SVGShareContainer = styled.div`
+  display: flex;
+  width: 100%;
+  height: 30%;
+  align-items: flex-end;
+  justify-content: flex-end;
+`;
+
+const SVGShare = styled.img`
+  width: 36px;
+  padding: 2%;
+  margin-right: 3%;
+  border: 2px solid #ff9233;
+  border-radius: 5px;
+`;
+
+const DynamicTitleContainer = styled.div`
+  position: absolute;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const zoom = keyframes`
+  0% {
+    transform: scale(1) rotateZ(0);
+  }
+  100% {
+    transform: scale(2.7) rotateZ(285deg); 
+  }
+`;
+const DynamicTitle = styled.p`
+  margin: 0;
+  font-size: 1000%;
+  color: #ff9233;
+  animation: ${zoom} 3s 0.2s forwards;
+`;
 
 function Home() {
-  return <p>Yo</p>;
+  const title = "goo";
+  return (
+    <PageWrapper>
+      <DynamicTitleContainer>
+        <DynamicTitle>{title.toUpperCase()}</DynamicTitle>
+      </DynamicTitleContainer>
+      <ButtonsContainer>
+        <SVGSettingContainer>
+          <SVGSetting src={gearFill} alt="gear-fill" />
+        </SVGSettingContainer>
+        <SVGPlayContainer>
+          <SVGPlay src={playCircle} alt="play-circle" />
+        </SVGPlayContainer>
+        <SVGShareContainer>
+          <SVGShare src={heartFill} alt="heart-fill" />
+        </SVGShareContainer>
+      </ButtonsContainer>
+    </PageWrapper>
+  );
 }
 
 export default Home;
